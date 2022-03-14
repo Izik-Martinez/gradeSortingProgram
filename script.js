e15968f0ec2055a0f668c4f1a34e3f56acc31192
Izik-Martinez <izikma17@yahoo.com>
2022-03-14 16:13:53 -0400

    Cleaned up code a little and removed unnecessary comments

--- script.js
+++ script.js
@@ -1,43 +1,36 @@
 let allGrades = [];
 
 
-let decider = "filler";
-// prompt("type 'new grade' or 'quit'")
+let decider = "fillerValue";
+
 
 
 
 while(decider !== "quit"){
 
-decider = prompt("type 'new' or 'quit'")
+ decider = prompt("type 'new' or 'quit'")
 
-if (decider === "new"){
+ if (decider === "new"){
 
-allGrades.push({
+ allGrades.push({
 
-name: prompt("type name here"),
-grade: Number(prompt("type grade here"))
+ name: prompt("type name here"),
+ grade: Number(prompt("type grade here"))
 
 })
 
-
 }
 
 
 
 
-// console.log(allGrades);
 
-}
 
-// console.log(decider);
+const passingGrades = allGrades.filter(n=>n.grade > 65)
+.map(n=>({name: n.name, grade: n.grade}));
 
-const passingGrades = allGrades.filter(n=>n.grade > 65).map(n=>({name: n.name, grade: n.grade}));
-const failingGrades = allGrades.filter(n=>n.grade < 65).map(n=>({name: n.name, grade: n.grade}));
+const failingGrades = allGrades.filter(n=>n.grade < 65)
+.map(n=>({name: n.name, grade: n.grade}));
+
 console.log(passingGrades);
 console.log(failingGrades);
-// allGrades.forEach(element => {
-//     element.name === "quit"
-//         let decider = "quit"
-//         console.log(decider);
-//     }
-// });
