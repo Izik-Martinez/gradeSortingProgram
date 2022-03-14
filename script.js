2ed46a770fd3a9ecdbfd59ddfca4ad098bed1061
Izik-Martinez <izikma17@yahoo.com>
2022-03-14 15:16:29 -0400

    Array declared. While loop to re-ask name and grade established. Working on closing loop

--- script.js
+++ script.js
@@ -1,2 +1,34 @@
-let hi = 5;
-console.log(hi);
+let allGrades = [];
+
+
+let decider = prompt("type 'new grade' or 'quit'")
+
+
+
+while(decider !== "quit"){
+
+
+
+if (decider === "new grade"){
+
+allGrades.push({
+
+name: prompt("type name here"),
+grade: Number(prompt("type grade here"))
+
+})
+
+allGrades.forEach(element => {
+    if (element.name === "quit"){
+        let decider = "quit"
+    }
+});
+
+}
+
+
+
+
+console.log(allGrades);
+
+}
